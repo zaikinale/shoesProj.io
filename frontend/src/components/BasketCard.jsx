93852e4ba19b42741eb4dd5df168a818c1fe3aa8
Alexei Zaikin <zaikinaleks04@mail.ru>
2026-02-02 21:22:48 +0300

    added load basket in client

--- frontend/src/components/BasketCard.jsx
+++ frontend/src/components/BasketCard.jsx
@@ -22,7 +22,7 @@ export default function BasketCard ({id, title, desc, price, image, type, quanti
                 <div className="basketInfo">
                     <h3 className="title">{title}</h3>
                     <span>{`Количество: ${quantity}`}</span>
-                    <span className="price">{`Общая цена: ${price*quantity} ₽`}</span>
+                    <span className="price">{`Общая цена: ${price * quantity} ₽`}</span>
                 </div>
             </div>
     )
--- frontend/src/pages/Basket.jsx
+++ frontend/src/pages/Basket.jsx
@@ -1,33 +1,52 @@
+import { getBasket } from '../api/basket.js';
+import { useEffect, useState } from 'react';
 import BasketCard from '../components/BasketCard.jsx'
 import NavigateTo from '../utils/navBtn.jsx'
-const testData = [
-    {
-        id: 1,
-        image: "https://i.ibb.co/qLhFKfGv/watch-10-46-rose-gold-light-blush-band-1-1000x1000.jpg",
-        title: 'test',
-        description: 'desc test good',
-        price: 3000,
-        quantity: 5,
-    },
-    {
-        id: 2,
-        image: "https://i.ibb.co/qLhFKfGv/watch-10-46-rose-gold-light-blush-band-1-1000x1000.jpg",
-        title: 'test1',
-        description: 'desc test good2',
-        price: 100,
-        quantity: 3,
-    },
-    {
-        id: 3,
-        image: "https://i.ibb.co/qLhFKfGv/watch-10-46-rose-gold-light-blush-band-1-1000x1000.jpg",
-        title: 'test21',
-        description: 'desc te213st good2',
-        price: 56600,
-        quantity: 10,
-    },
-]
+// const testData = [
+//     {
+//         id: 1,
+//         image: "https://i.ibb.co/qLhFKfGv/watch-10-46-rose-gold-light-blush-band-1-1000x1000.jpg",
+//         title: 'test',
+//         description: 'desc test good',
+//         price: 3000,
+//         quantity: 5,
+//     },
+//     {
+//         id: 2,
+//         image: "https://i.ibb.co/qLhFKfGv/watch-10-46-rose-gold-light-blush-band-1-1000x1000.jpg",
+//         title: 'test1',
+//         description: 'desc test good2',
+//         price: 100,
+//         quantity: 3,
+//     },
+//     {
+//         id: 3,
+//         image: "https://i.ibb.co/qLhFKfGv/watch-10-46-rose-gold-light-blush-band-1-1000x1000.jpg",
+//         title: 'test21',
+//         description: 'desc te213st good2',
+//         price: 56600,
+//         quantity: 10,
+//     },
+// ]
 
 export default function Basket () {
+    const [goods, setGoods] = useState([]);
+
+    const loadGoods = async () => {
+        try {
+            const data = await getBasket();
+            console.log(data)
+            setGoods(data.items);
+        } catch (error) {
+            console.error('Error loading goods: ', error);
+        }
+    };
+
+    useEffect(() => {
+        loadGoods();
+    }, []);
+
+
     const orderBlock = () => {
         const totalPrise = 10000;
         const totalCount = 5;
@@ -44,18 +63,18 @@ export default function Basket () {
         if (type === "user") {
             return (
                 <div className="containerColumn">
-                    {testData.length > 0 ? (
+                    {goods.length > 0 ? (
                         <>
-                            {testData.map((good) => (
+                            {goods.map((goodItem) => (
                                 <BasketCard
-                                    key={good.id}
-                                    id={good.id}
-                                    title={good.title}
-                                    desc={good.description}
-                                    price={good.price}
-                                    image={good.image}
+                                    key={goodItem.id}
+                                    id={goodItem.id}
+                                    title={goodItem.good.title}
+                                    desc={goodItem.good.description}
+                                    price={goodItem.good.price}
+                                    image={goodItem.good.image}
                                     type={type}
-                                    quantity={good.quantity}
+                                    quantity={goodItem.quantity}
                                 />
                             ))}
                             {orderBlock()}
